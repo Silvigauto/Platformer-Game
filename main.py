900323eea17515d49eef376c651ed7d5a5180426
Silvina Gauto <silvigauto02@gmail.com>
2026-01-13 10:59:57 -0300

    feat: add main menu and start and exit buttons

--- main.py
+++ main.py
@@ -20,10 +20,14 @@ pygame.display.set_caption('Platformer')
 background_image = pygame.image.load("Recursos\\fondo2.jpg")
 background_image = pygame.transform.smoothscale(background_image, (screen_width, screen_height))
 restart_img = pygame.image.load('Recursos\\buttons\\restart_btn.png')
+start_img = pygame.image.load('Recursos\\buttons\\start_btn.png')
+exit_img = pygame.image.load('Recursos\\buttons\\exit_btn.png')
+
 
 #define game variables
 tile_size = 100
 game_over = 0 
+main_menu = True
 
 
 
@@ -48,6 +52,8 @@ world = World(world_data, tile_size, ghost_group,lava_group,screen)
 player = Player(100, screen_height - 180)
 
 restart_button = Button(screen_width // 2 - 50, screen_height // 2 + 100, restart_img)
+start_button = Button(screen_width // 2 - 350, screen_height // 2, start_img)
+exit_button = Button(screen_width // 2 + 150, screen_height // 2, exit_img)
 
 
 run = True
@@ -55,24 +61,31 @@ while run:
     clock.tick(fps)
     screen.blit(background_image, (0,0))
 
-    world.draw(screen)
+    if main_menu:
+        if exit_button.draw(screen):
+            run = False
+        if start_button.draw(screen):
+            main_menu = False
+    else:
+        #game logic
+        world.draw(screen)
 
-    if game_over == 0:
-        ghost_group.update() #when go is -1 it stops moving
+        if game_over == 0:
+            ghost_group.update() #when go is -1 it stops moving
 
-    ghost_group.draw(screen) #but it doesnt stop showing
-    lava_group.draw(screen)
+        ghost_group.draw(screen) #but it doesnt stop showing
+        lava_group.draw(screen)
 
-    for ghost in ghost_group:
-        pygame.draw.rect(screen, (255,255,255), ghost.rect, 2) #draw it here so it stays showing
+        for ghost in ghost_group:
+            pygame.draw.rect(screen, (255,255,255), ghost.rect, 2) #draw it here so it stays showing
 
-    game_over =  player.update(screen, world, ghost_group, lava_group, game_over)
+        game_over =  player.update(screen, world, ghost_group, lava_group, game_over)
 
-    #if player has died
-    if game_over == -1:
-        if restart_button.draw(screen):
-            player.reset(100, screen_height - 180)
-            game_over = 0
+        #if player has died
+        if game_over == -1:
+            if restart_button.draw(screen):
+                player.reset(100, screen_height - 180)
+                game_over = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
